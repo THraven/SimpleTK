870f569ee5821c143c93ce06a5f51bbb551a5a6c
THraven <h_raven@live.nl>
2019-05-14 14:41:02 +0200

    added InitLabel method to framework.py

--- framework.py
+++ framework.py
@@ -5,10 +5,9 @@
 class SimpleTk():
     """the container class for all functions used"""
 
-    def InitButton(self, name=None, text="", command=None):
+    def InitButton(self, name="button%s" % len(self.button), text="", command=None):
         """this is for the easy making of buttons in the app.
         and will store some stuff in the """
-        # checking if the name is given, if not it will just generate a name
         if name == None:
             name = "button%s" % len(self.button)
         DisplayText = tk.StringVar()
@@ -17,3 +16,18 @@ class SimpleTk():
         self.button.update({name: {"button": ButtonInctence,
                                     "text": DisplayText}})
         self.button[name]["button"].pack()
+
+    def InitLabel(self, name=None, text=None):
+        """this is for the easy making of labels in the app.
+        it will store this such as name and text"""
+        if name == None:
+            name = "label%s" % len(self.label)
+        DisplayText = tk.StringVar()
+        if not type(text) == type(DisplayText):
+            DisplayText.set(text)
+        else:
+            DisplayText = text
+        LabelInctence = tk.Label(self.window, textvariable=DisplayText)
+        self.label.update({name: {"label": LabelInctence,
+                                    "text": DisplayText}})
+        self.label[name]["label"].pack()
