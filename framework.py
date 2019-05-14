f16b486a93f69ce2da51a7883f52368de1d98f55
THraven <h_raven@live.nl>
2019-05-14 13:01:50 +0200

    added InitButton method to framework.py

--- framework.py
+++ framework.py
@@ -5,4 +5,15 @@
 class SimpleTk():
     """the container class for all functions used"""
 
-    
+    def InitButton(self, name=None, text="", command=None):
+        """this is for the easy making of buttons in the app.
+        and will store some stuff in the """
+        # checking if the name is given, if not it will just generate a name
+        if name == None:
+            name = "button%s" % len(self.button)
+        DisplayText = tk.StringVar()
+        DisplayText.set(text)
+        ButtonInctence = tk.Button(self.window, textvariable=DisplayText, command=command)
+        self.button.update({name: {"button": ButtonInctence,
+                                    "text": DisplayText}})
+        self.button[name]["button"].pack()
---
+++ functions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+"""you can put all the functions for buttons, menus and other stuff here"""
+
+import framework
+
+class function(framework):
+    """put functions here"""
+
+    
